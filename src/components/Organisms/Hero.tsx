747c80ca9131a1d80d110160c7a1c13f8e39c097
flotsam <mpb1@e.mail.de>
2025-10-18 21:42:14 +0200

    chore(Hero.tsx): cleanup redundant code

--- src/components/Organisms/Hero.tsx
+++ src/components/Organisms/Hero.tsx
@@ -36,18 +36,6 @@ export default function Hero({ onGetStarted }: HeroProps) {
               <div className={styles.screen}>
                 <div className={styles.screenContent}>
                   <img src={heroImg} alt="Hero image of the app in mobile view" />
-                  {/* <div className={styles.stat}>
-                    <span className={styles.label}>Calories</span>
-                    <span className={styles.value}>1,847</span>
-                  </div>
-                  <div className={styles.stat}>
-                    <span className={styles.label}>Protein</span>
-                    <span className={styles.value}>92g</span>
-                  </div>
-                  <div className={styles.stat}>
-                    <span className={styles.label}>Carbs</span>
-                    <span className={styles.value}>215g</span>
-                  </div> */}
                 </div>
               </div>
             </div>
