a1da9df074663702131047e68b1e62393cb960ba
flotsam <mpb1@e.mail.de>
2025-10-18 22:36:59 +0200

    fix: remove escaping from express-validator to allow ampersand in name

--- backend/src/schemas/newProductSchema.ts
+++ backend/src/schemas/newProductSchema.ts
@@ -2,19 +2,19 @@ import {body} from "express-validator";
 
 export const productSchema = [
     body("name")
-        .exists().escape().trim().isLength({min:2, max:40}).withMessage("The name must be between 2 and 40 characters long!"),
+        .exists().trim().isLength({min:2, max:40}).withMessage("The name must be between 2 and 40 characters long!"),
     body("calories")
-        .exists().escape().trim().isNumeric().withMessage("Calories cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Calories cannot be empty and must be a number!"),
     body("carbohydrates")
-        .exists().escape().trim().isNumeric().withMessage("Carbohydrates cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Carbohydrates cannot be empty and must be a number!"),
     body("fat")
-        .exists().escape().trim().isNumeric().withMessage("Fat cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Fat cannot be empty and must be a number!"),
     body("protein")
-        .exists().escape().trim().isNumeric().withMessage("Protein cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Protein cannot be empty and must be a number!"),
     body("saturatedFat")
-        .exists().escape().trim().isNumeric().withMessage("Saturated fat cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Saturated fat cannot be empty and must be a number!"),
     body("sugar")
-        .exists().escape().trim().isNumeric().withMessage("Sugar cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Sugar cannot be empty and must be a number!"),
     body("salt")
-        .exists().escape().trim().isNumeric().withMessage("Salt cannot be empty and must be a number!"),
+        .exists().trim().isNumeric().withMessage("Salt cannot be empty and must be a number!"),
 ];
